6b66785fa8ed827b8232d9b3c817898d46fbd60f
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-01 00:52:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,25 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  values_array = []
+  name_hash.collect do |name, value|
+    values_array << value
+  end
+   swapped = true
+   while swapped do
+     swapped = false
+     (values_array.length - 1).times do |i|
+       if values_array[i] < values_array[i + 1]
+         values_array[i], values_array[i + 1] = values_array[i + 1], values_array[i]
+     swapped = true
+       end
+     end
+   end
+   values_array
+   max = values_array.first
+   name_hash.collect do |name, value|
+    if value == max
+      return name
+      end
+      end
 end
